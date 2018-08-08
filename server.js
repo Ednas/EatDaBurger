88b80000a59e7cebd1e0f6b292843bf25842d1f7
Ednas <edna@edna.tech>
2018-08-08 08:05:00 -0400

    Reconfigured haltontimedout

--- server.js
+++ server.js
@@ -8,22 +8,23 @@ var app = express();
 //Express static uses whatever domain name/public
 // app.use(express.static(__dirname + '/public'));
 app.use("/static", express.static("public")); //Handlebars likes this one better
-
-//This sets up body-parser
-// parse application/x-www-form-urlencoded 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//This is setting up method-override 
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
-
 // Timeout
-app.use(timeout(10000));
+app.use(timeout(15000));
 app.use(haltOnTimedout);
 
 function haltOnTimedout(req, res, next) {
     if (!req.timedout) next();
 }
+
+//This sets up body-parser
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(haltOnTimedout);
+
+//This is setting up method-override 
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+app.use(haltOnTimedout);
 
 //sets up express-handlebars
 var exphbs = require('express-handlebars');
@@ -38,7 +39,7 @@ var routes = require('./controllers/burgers_controllers.js');
 app.use('/', routes);
 app.use('/update', routes);
 app.use('/create', routes);
-
+app.use(haltOnTimedout);
 
 //sets up the port as a variable and listens to it
 var port = process.env.PORT || 3000;
